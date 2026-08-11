64503bb67f5431262953b7544c15a36925a1aa01
icrc-loliveira <loliveira@icrc.org>
2026-08-11 14:08:08 +0100

    Added support to "System Developer"

--- src/controllers/appointmentsHeaderController.js
+++ src/controllers/appointmentsHeaderController.js
@@ -6,11 +6,14 @@ angular.module('bahmni.appointments')
             var setBackLinks = function () {
                 var backLinks = [{label: "Home", url: Bahmni.Appointments.Constants.homeUrl, accessKey: "h", icon: "fa-home"}];
 
-                // TODO:permissions for admin
+                // Verifica se o utilizador tem o privilégio de Admin OU de System Developer
+                var hasAdminPrivilege = appService.getAppDescriptor().getConfigValue('requiredPrivilege') ||
+                    [Bahmni.Appointments.Constants.privilegeForAdmin, 'System Developer'];
+
                 backLinks.push({text: "APPOINTMENTS_MANAGE", state: "home.manage", accessKey: "M"});
                 var enableAdminPage = appService.getAppDescriptor().getExtensionById('bahmni.appointments.admin', true);
                 if (enableAdminPage) {
-                    backLinks.push({text: "APPOINTMENTS_ADMIN", state: "home.admin.service", accessKey: "A", requiredPrivilege: Bahmni.Appointments.Constants.privilegeForAdmin});
+                    backLinks.push({text: "APPOINTMENTS_ADMIN", state: "home.admin.service", accessKey: "A", requiredPrivilege: hasAdminPrivilege});
                 }
                 $state.get('home').data.backLinks = backLinks;
             };

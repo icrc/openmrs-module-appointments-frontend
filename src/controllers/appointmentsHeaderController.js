f2f6ad04d4366b100afb6a5ca1cdb6d3af01e0f2
psousa@icrc.org <icrc-psousa>
2026-09-10 10:37:15 +0100

    Allow System Developer to admin appointments

--- src/controllers/appointmentsHeaderController.js
+++ src/controllers/appointmentsHeaderController.js
@@ -1,19 +1,32 @@
 'use strict';
 
 angular.module('bahmni.appointments')
-    .controller('AppointmentsHeaderController', ['$scope', '$state', 'appService',
-        function ($scope, $state, appService) {
+    .controller('AppointmentsHeaderController', ['$scope', '$state', 'appService', 'appointmentCommonService', 'currentSessionUser',
+        function ($scope, $state, appService, appointmentCommonService, currentSessionUser) {
             var setBackLinks = function () {
                 var backLinks = [{label: "Home", url: Bahmni.Appointments.Constants.homeUrl, accessKey: "h", icon: "fa-home"}];
 
-                // Verifica se o utilizador tem o privilégio de Admin OU de System Developer
-                var hasAdminPrivilege = appService.getAppDescriptor().getConfigValue('requiredPrivilege') ||
-                    [Bahmni.Appointments.Constants.privilegeForAdmin, 'System Developer'];
+                var appointmentsPrivilegeForAdmin = Bahmni.Appointments.Constants.privilegeForAdmin;
+                var hasAppointmentsAdminPrivilege = appointmentCommonService.isCurrentUserHavingPrivilege(appointmentsPrivilegeForAdmin, $scope.currentUser.privileges);
+
+                var enableAdminPage = appService.getAppDescriptor().getExtensionById('bahmni.appointments.admin', true);
 
                 backLinks.push({text: "APPOINTMENTS_MANAGE", state: "home.manage", accessKey: "M"});
-                var enableAdminPage = appService.getAppDescriptor().getExtensionById('bahmni.appointments.admin', true);
+
                 if (enableAdminPage) {
-                    backLinks.push({text: "APPOINTMENTS_ADMIN", state: "home.admin.service", accessKey: "A", requiredPrivilege: hasAdminPrivilege});
+                    if (hasAppointmentsAdminPrivilege) {
+                        backLinks.push({text: "APPOINTMENTS_ADMIN", state: "home.admin.service", accessKey: "A", requiredPrivilege: appointmentsPrivilegeForAdmin});
+                    } else {
+                        var roles = currentSessionUser && currentSessionUser.roles ? currentSessionUser.roles : [];
+
+                        var isSystemDeveloper = roles.some(function (role) {
+                            return role.display === 'System Developer' || role.name === 'System Developer';
+                        });
+
+                        if (isSystemDeveloper) {
+                            backLinks.push({text: "APPOINTMENTS_ADMIN", state: "home.admin.service", accessKey: "A"});
+                        }
+                    }
                 }
                 $state.get('home').data.backLinks = backLinks;
             };
